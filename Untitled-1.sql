1a8411b726699cf7d879b794944f8eb966e9e1f2
Aditya Vyawahare <AdityaJagdishVyawahare@gmail.com>
2021-02-02 21:13:50 +0530

    sample .sql files

---
+++ Untitled-1.sql
@@ -0,0 +1,3 @@
+-- INSERT INTO photos(image_url,movie_id) VALUES
+-- ('https://m.media-amazon.com/images/M/MV5BNmM2MWQ0NzktNzU0OS00MjYzLTkxNDYtMzliNTA5ZmNkMmZlXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg', 'tt10451914')
+select * from photos;
